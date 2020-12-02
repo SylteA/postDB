ec5449c4aa651eb44f97c3d28acd89be1fa2340a
Sylte <sylteandreas7@gmail.com>
2020-12-02 21:36:00 +0100

    Try to release first final version.
    Signed-off-by: Sylte <sylteandreas7@gmail.com>

--- postDB/__init__.py
+++ postDB/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.0.4a"
+__version__ = "0.1"
 __author__ = "Sylte"
 __licence__ = "MIT"
 __copyright__ = "Copyright (c) 2020 Sylte"
@@ -18,7 +18,7 @@ def get_version(version: tuple):
     return "%s %s.%s.%s" % (version[3], version[0], version[1], version[2])
 
 
-VERSION = (0, 0, 4, "alpha", 0)
+VERSION = (0, 1, 0, "final", 0)
 version_info = VersionInfo(*VERSION)
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
