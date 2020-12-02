e2ac8004599f3d3a3e96872e16447a59d252bd48
Sylte <sylteandreas7@gmail.com>
2020-12-02 19:09:55 +0100

    Move __ - attrs
    Signed-off-by: Sylte <sylteandreas7@gmail.com>

--- postDB/__init__.py
+++ postDB/__init__.py
@@ -1,3 +1,9 @@
+__version__ = "0.0.2a"
+__author__ = "Sylte"
+__licence__ = "MIT"
+__copyright__ = "Copyright (c) 2020 Sylte"
+__title__ = "postDB"
+
 import logging
 from collections import namedtuple
 
@@ -9,17 +15,10 @@ VersionInfo = namedtuple("VersionInfo", "major minor micro releaselevel serial")
 
 def get_version(version: tuple):
     """Return a cleaned up version number from VERSION."""
-    return "%s.%s.%s%s" % (version[0], version[1], version[2], version[3][0])
+    return "%s %s.%s.%s" % (version[3], version[0], version[1], version[2])
 
 
 VERSION = (0, 0, 2, "alpha", 0)
 version_info = VersionInfo(*VERSION)
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
-
-
-__version__ = get_version(VERSION)
-__author__ = "Sylte"
-__licence__ = "MIT"
-__copyright__ = "Copyright (c) 2020 Sylte"
-__title__ = "postDB"
